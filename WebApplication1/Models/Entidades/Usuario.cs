2422280df6b4c92ae781382ddece72ddcfbd66b7
Arturo Almeida <arturo_ac97@hotmail.com>
2019-05-22 21:27:56 -0500

    Sistema Crud

---
+++ WebApplication1/Models/Entidades/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Entidades
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Description ="Codigo")]     
+        public int Id { get; set; }
+
+        [Display(Description = "Nombre de usuario")]  
+        public string NombreUsuario { get; set; }
+
+        [Display(Description = "Edad")]
+        public int Edad { get; set; }
+
+        [Display(Description = "Tipo de usuario")]
+        public int Tipo { get; set; }
+    }
+}
---
+++ dbo.Usuario.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Usuario] (
+    [Id]            INT          IDENTITY (1, 1) NOT NULL,
+    [NombreUsuario] VARCHAR (50) NULL,
+    [Edad]          INT          NULL,
+    [Tipo]          INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
